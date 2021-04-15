f985b31b7f2168fc0c44ec928699c1f8799071a3
Michal Dvorak <michal@mdvorak.org>
2021-04-15 12:05:41 +0200

    status_led according to websocket connection state

--- main/app_status.c
+++ main/app_status.c
@@ -1,9 +1,9 @@
 #include <esp_event.h>
 #include <esp_log.h>
 #include <esp_websocket_client.h>
+#include <freertos/event_groups.h>
 #include <status_led.h>
 #include <wifi_provisioning/manager.h>
-#include <wifi_reconnect.h>
 
 static const char TAG[] = "app_status";
 
@@ -11,11 +11,18 @@ static const uint32_t STATUS_LED_CONNECTING_INTERVAL = 500u;
 static const uint32_t STATUS_LED_PROV_INTERVAL = 50u;
 static const uint32_t STATUS_LED_READY_INTERVAL = 100u;
 
+static EventGroupHandle_t event_group = NULL;
+
+#define WS_CONNECTED_BIT BIT0
+
 static void connected_handler(__unused void *handler_arg, __unused esp_event_base_t event_base,
                               __unused int32_t event_id, __unused void *event_data)
 {
-    status_led_handle_ptr status_led = (status_led_handle_ptr)handler_arg;
+    // State
+    xEventGroupSetBits(event_group, WS_CONNECTED_BIT);
 
+    // LED
+    status_led_handle_ptr status_led = (status_led_handle_ptr)handler_arg;
     uint32_t timeout_ms = STATUS_LED_READY_INTERVAL * 6;
     status_led_set_interval_for(status_led, STATUS_LED_READY_INTERVAL, false, timeout_ms, false);
 }
@@ -23,6 +30,10 @@ static void connected_handler(__unused void *handler_arg, __unused esp_event_bas
 static void disconnected_handler(__unused void *handler_arg, __unused esp_event_base_t event_base,
                                  __unused int32_t event_id, __unused void *event_data)
 {
+    // State
+    xEventGroupClearBits(event_group, WS_CONNECTED_BIT);
+
+    // LED
     status_led_handle_ptr status_led = (status_led_handle_ptr)handler_arg;
     status_led_set_interval(status_led, STATUS_LED_CONNECTING_INTERVAL, true);
 }
@@ -38,26 +49,30 @@ static void wifi_prov_handler(__unused void *handler_arg, __unused esp_event_bas
     }
     else if (event_id == WIFI_PROV_END)
     {
-        if (!wifi_reconnect_is_connected())
+        // If not already connected
+        if ((xEventGroupGetBits(event_group) & WS_CONNECTED_BIT) == 0)
         {
             status_led_set_interval(status_led, STATUS_LED_CONNECTING_INTERVAL, true);
         }
     }
 }
 
-// TODO     ESP_ERROR_CHECK(esp_websocket_register_events(client, WEBSOCKET_EVENT_ANY, websocket_event_handler, nullptr));
-
 void app_status_init(esp_websocket_client_handle_t client)
 {
+    event_group = xEventGroupCreate();
+
     // Status LED
     esp_err_t err = status_led_create_default();
     if (err == ESP_OK)
     {
+        // Start flashing
         ESP_ERROR_CHECK_WITHOUT_ABORT(status_led_set_interval(STATUS_LED_DEFAULT, STATUS_LED_CONNECTING_INTERVAL, true));
 
-        ESP_ERROR_CHECK_WITHOUT_ABORT(esp_event_handler_instance_register(IP_EVENT, IP_EVENT_STA_GOT_IP, connected_handler, STATUS_LED_DEFAULT, NULL));
-        ESP_ERROR_CHECK_WITHOUT_ABORT(esp_event_handler_instance_register(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, disconnected_handler, STATUS_LED_DEFAULT, NULL));
+        // Register event handlers
         ESP_ERROR_CHECK_WITHOUT_ABORT(esp_event_handler_instance_register(WIFI_PROV_EVENT, ESP_EVENT_ANY_ID, wifi_prov_handler, STATUS_LED_DEFAULT, NULL));
+        ESP_ERROR_CHECK_WITHOUT_ABORT(esp_event_handler_instance_register(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, disconnected_handler, STATUS_LED_DEFAULT, NULL));
+        ESP_ERROR_CHECK_WITHOUT_ABORT(esp_websocket_register_events(client, WEBSOCKET_EVENT_DISCONNECTED, disconnected_handler, STATUS_LED_DEFAULT));
+        ESP_ERROR_CHECK_WITHOUT_ABORT(esp_websocket_register_events(client, WEBSOCKET_EVENT_CONNECTED, connected_handler, STATUS_LED_DEFAULT));
     }
     else
     {
