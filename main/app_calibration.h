18848654796172394e2770501be06e1eca2e21bc
Michal Dvorak <mikee2185@gmail.com>
2021-12-28 19:42:26 +0100

    use cal defaults now

--- main/app_calibration.h
+++ main/app_calibration.h
@@ -2,11 +2,4 @@
 
 #include <mpu9250.h>
 
-static calibration_t cal = {
-    .mag_offset = {.x = 0, .y = 0, .z = 0},
-    .mag_scale = {.x = 1, .y = 1, .z = 1},
-    .gyro_bias_offset = {.x = 0, .y = 0, .z = 0},
-    .accel_offset = {.x = 0, .y = 0, .z = 0},
-    .accel_scale_lo = {.x = 1, .y = 1, .z = 1},
-    .accel_scale_hi = {.x = 1, .y = 1, .z = 1},
-};
+static calibration_t cal = CALIBRATION_DEFAULTS();
